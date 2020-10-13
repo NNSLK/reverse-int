3985f9a645d5b4a28524137fd761f7625ef581d8
NNSLK <gogamemann@gmail.com>
2020-10-13 16:54:14 +0300

    init: start reverse task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        let y = -n;
+        return y.toFixed(0).split("").reverse().join("") - 0;
+    } else if (n > 0) {
+        return n.toFixed(0).split("").reverse().join("") - 0;
+    }
+};
